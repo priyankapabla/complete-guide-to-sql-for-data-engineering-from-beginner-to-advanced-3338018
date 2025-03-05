9c9e3081e3cbd292cfb9228756eb1c169b59f3af
Priyanka saini <113256585+priyankapabla@users.noreply.github.com>
2025-03-05 14:16:32 +0000

    committed

--- 02_04.sql
+++ 02_04.sql
@@ -1,10 +1,20 @@
-select * from products order by price;
+select *
+from products
+order by price;
 
-select * from customer order by customer_name;
+select *
+from customer
+order by customer_name;
 
+select *
+from customer
+order by customer_name desc;
 
-select * from customer order by customer_name desc;
+select *
+from products
+order by price, name;
 
-select * from products order by price, name;
-
-select * from products where is not price>100  order by price;
+select *
+from products
+where price > 100
+order by price;
