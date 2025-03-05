c57180a7dcb854009694acb3f5cf58552d0e0e0b
Priyanka saini <113256585+priyankapabla@users.noreply.github.com>
2025-03-05 14:29:36 +0000

    commit

--- 03_02.sql
+++ 03_02.sql
@@ -1,7 +1,8 @@
 select * from categories;
 
-select * from products
-INSERT INTO Products (name, price, description, tags, category_id,Supplier)
+select * from products;
+
+INSERT INTO products (name, price, description, tags, category_id,Supplier)
 VALUES
-    ('Ipad', 100, 'High-performance ipad for professionals', 'electronics, portable, tech', 1, 'SupplierA')
+    ('Ipad', 100, 'High-performance ipad for professionals', 'electronics, portable, tech', 1, 'SupplierA');
 	
